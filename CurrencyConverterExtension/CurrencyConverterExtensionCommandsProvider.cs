8e6308cbd2b71e49b7288cee8e1134ac7e1e899f
Advaith A J <advaitharunjeena@gmail.com>
2025-04-09 20:44:10 +0400

    Settings added

--- CurrencyConverterExtension/CurrencyConverterExtensionCommandsProvider.cs
+++ CurrencyConverterExtension/CurrencyConverterExtensionCommandsProvider.cs
@@ -2,6 +2,7 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using CurrencyConverterExtension.Helpers;
 using Microsoft.CommandPalette.Extensions;
 using Microsoft.CommandPalette.Extensions.Toolkit;
 
@@ -10,13 +11,16 @@ namespace CurrencyConverterExtension;
 public partial class CurrencyConverterExtensionCommandsProvider : CommandProvider
 {
     private readonly ICommandItem[] _commands;
+    private readonly SettingsManager _settingsManager = new();
 
     public CurrencyConverterExtensionCommandsProvider()
     {
         DisplayName = "Currency Converter";
         Icon = IconHelpers.FromRelativePath("Assets\\StoreLogo.png");
+        Settings = _settingsManager.Settings;
+        string Subtitle = "Convert real and crypto currencies.";
         _commands = [
-            new CommandItem(new CurrencyConverterExtensionPage()) { Title = DisplayName },
+            new CommandItem(new CurrencyConverterExtensionPage()) { Title = DisplayName, Icon = Icon, Subtitle = Subtitle },
         ];
     }
 
---
+++ CurrencyConverterExtension/Helpers/SettingsManager.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using CurrencyConverterExtension.Properties;
+using Microsoft.CommandPalette.Extensions.Toolkit;
+
+namespace CurrencyConverterExtension.Helpers;
+
+public class SettingsManager : JsonSettingsManager
+{
+    private static readonly string _namespace = "currency-converter";
+
+    private static string Namespaced(string propertyName) => $"{_namespace}.{propertyName}";
+
+    private readonly ChoiceSetSetting _outputStyle = new(
+        Namespaced(nameof(OutputStyle)),
+        Resources.output_style,
+        Resources.output_style_description,
+        new()
+        {
+            new(Resources.output_style_short_text, "0"),
+            new(Resources.output_style_full_text, "1"),
+        })
+    { Value = "1" };
+
+    public int OutputStyle => int.Parse(_outputStyle.Value);
+
+    internal static string SettingsJsonPath()
+    {
+        var dir = Utilities.BaseSettingsPath("Microsoft.CmdPal");
+        Directory.CreateDirectory(dir);
+
+        return Path.Combine(dir, "currency-converter.json");
+    }
+
+    public SettingsManager()
+    {
+        FilePath = SettingsJsonPath();
+
+        Settings.Add(_outputStyle);
+
+        // Load settings from file upon initialization
+        LoadSettings();
+
+        Settings.SettingsChanged += (s, a) => this.SaveSettings();
+    }
+}
